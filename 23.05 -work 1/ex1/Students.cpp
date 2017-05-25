6e77461ebcb5765f4c509d8350f2a098e33de0ee
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-05-25 15:28:55 +0300

    Create Students.cpp

---
+++ 23.05 -work 1/ex1/Students.cpp
@@ -0,0 +1,78 @@
+
+#include "Students.h"
+
+
+ void Students::printl(std::string la)
+{
+	std::cout << la;
+}
+
+
+std::string Students::getfio()const {
+	return fio;
+};
+
+void Students::setfio(std::string name)
+{
+	fio = name;
+}
+
+
+
+std::string Students::getyears()const
+{
+	return years;
+}
+void Students::setyears(std::string n)
+{
+	years = n;
+}
+
+
+
+
+std::string Students::getphone()const
+{
+	return phone;
+}
+void Students::setphone(std::string tel)
+{
+	phone = tel;
+}
+
+
+
+
+std::string Students::getcity()const
+{
+	return city;
+}
+void Students::setcity(std::string gorod)
+{
+	city = gorod;
+}
+
+
+
+std::string Students::getschool()const
+{
+	return school;
+}
+
+void Students::setschool(std::string sh)
+{
+	school = sh;
+}
+
+
+
+
+std::string Students::getn_g()const
+{
+	return n_g;
+}
+
+void Students::setn_g(std::string group)
+{
+	n_g = group;
+}
