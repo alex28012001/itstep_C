12eee76236800457bd5dba4781b4463d5b46c034
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-05-30 21:18:00 +0300

    Create MyVector.cpp

---
+++ cpp_3/MyVector.cpp
@@ -0,0 +1,97 @@
+#include "MyVector.h"
+#include<iostream>
+
+
+bool MyVector::empty()const
+{
+	bool flag;
+	if (size <= capacity)
+		flag = true;
+	else
+		flag = false;
+
+	return flag;
+}
+
+
+double MyVector::get_size()const
+{
+	return size;
+}
+
+
+
+double MyVector::get_capacity()const
+{
+	return capacity;
+}
+
+
+
+
+void MyVector::push_back(double a)
+{
+	if (size < capacity)
+	{
+		arr[size + 1];
+		arr[size] = a;
+
+	}
+
+	else 
+	{
+		double * ptr;
+		ptr = new double[capacity * 2];
+
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = ptr[i];
+		}
+
+		arr[size + 1];	
+		arr[size] = a;
+
+
+	}
+
+	std::cout << arr[size];
+}
+
+
+
+
+double MyVector::pop_back()
+{
+
+	if (!size)std::cout << "Vector is empty!!!";
+	else --size;
+	return arr[size];
+}
+
+
+
+double& MyVector::at(const int a)
+{
+	if (a < size)
+	{
+		return arr[a];
+	}
+
+	else
+		std::cout << "Error";
+}
+
+
+
+void MyVector::print() const
+{
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+
+
+
