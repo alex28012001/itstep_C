82969f43ec8379eef9fcde0159e35678255fa9d4
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-01 20:31:15 +0300

    Update Text.cpp

--- cpp_4a/Text.cpp
+++ cpp_4a/Text.cpp
@@ -3,14 +3,193 @@
 #include<fstream>
 
 
-std::string Text :: get_max_len()const
+
+void Text::f(std::string name)
+{
+	std::ifstream  filein("test_text.txt");
+	if (!filein.is_open())
+	{
+		std::cout << "File not found\n";
+	}
+
+	std::string tmp;
+	while (filein >> tmp)
+	{
+		++size;
+	}
+	str = new std::string[size];
+	filein.clear();
+	filein.seekg(0);
+
+	for (int i = 0; i < size; ++i)
+	{
+		filein >> tmp;
+		str[i] = tmp;
+		if (str[i].length() > maxlen)
+			maxlen = str[i].length();
+	}
+
+	filein.close();
+}
+
+
+
+Text::Text () 
+{
+	f("test_text.txt");
+};
+
+Text::Text (int count)
+{
+	std::ifstream filein("test_text.txt");
+	if (!filein.is_open())
+	{
+		std::cout << "File not found\n";
+	}
+
+
+
+	std::string tmp;
+	while (filein >> tmp)
+	{
+		++size;
+	}
+
+	if (count < 0 || count > size)
+	{
+		std::cout << "Error";
+	}
+
+
+
+
+	else
+	{
+
+		str = new std::string[count];
+		filein.clear();
+		filein.seekg(0);
+
+		for (int i = 0; i < count; ++i)
+		{
+			filein >> tmp;
+			str[i] = tmp;
+		}
+
+
+
+	}
+
+
+
+
+
+	filein.close();
+};
+
+Text::Text( std::string name)
+{
+	f("test_text.txt");
+};
+
+Text::Text( int count,  std::string name)
 {
 
 
+
+	std::ifstream filein(name);
+	if (!filein.is_open())
+	{
+		std::cout << "File not found\n";
+	}
+
+
+
+	std::string tmp;
+	while (filein >> tmp)
+	{
+		++size;
+	}
+
+	if (count < 0 || count > size)
+	{
+		std::cout << "Error";
+	}
+
+
+
+
+	else
+	{
+
+		str = new std::string[count];
+		filein.clear();
+		filein.seekg(0);
+
+		for (int i = 0; i < count; ++i)
+		{
+			filein >> tmp;
+			str[i] = tmp;
+		}
+
+
+
+	}
+
+
+
+
+
+	filein.close();
+
+
+};
+
+
+
+
+
+
+Text::Text(const Text &a) : size(a.size)
+{
+	str = new std::string[size];
+
+	for (int i = 0; i < size; ++i)
+	{
+		str[i] = a.str[i];
+	}
+
+};
+
+Text::Text(Text&&a) : size(a.size), str(a.str)
+{
+	a.size = 0;
+	a.str = nullptr;
+};
+
+
+
+
+
+int Text :: get_max_len()const
+{
+  return maxlen;
+
 }
+
+
+
+/*
 
 std::string Text :: get_max_word()const
 {
+	int  maxlen = 0, index = 0;
 
+	for (int i = 0;i<size; ++i)
+	{
+		
 
+	}
+		
 }
+*/
