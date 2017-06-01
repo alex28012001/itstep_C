6e7129eacf39998ac1c26ca3e1eccce7ddce5bdc
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-01 21:17:10 +0300

    Create Digit.h

---
+++ overload operators/Digit.h
@@ -0,0 +1,27 @@
+#pragma once
+class Digit
+{
+	int digit;
+
+
+public:
+	Digit():digit(1) {};
+	explicit Digit(const int x) :digit(x) {};
+	int get_digit()const;
+
+	~Digit() {};
+
+	Digit operator+(const Digit x)
+	{
+		Digit tmp;
+		tmp.digit = digit + x.digit;
+		return tmp;
+	}
+
+	Digit operator+(const int x)
+	{
+		Digit tmp;
+		tmp.digit = digit + x;
+		return tmp;
+	}
+};
