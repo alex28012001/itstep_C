6a9a4187967ffd3bd2c8905941d52ee1e02faccd
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-05 12:51:56 +0300

    Create Digit.cpp

---
+++ lab 5/Digit.cpp
@@ -0,0 +1,188 @@
+#include "Digit.h"
+size_t Digit::counter = 0;
+
+int Digit::get_digit()const
+{
+	return digit;
+}
+
+void Digit::set_digit(const int x)
+{
+	digit = x;
+}
+
+Digit Digit:: operator+(const Digit x)
+{
+	Digit tmp;
+	tmp.digit = digit + x.digit;
+	return tmp;
+}
+
+Digit Digit::operator+(const int x)
+{
+	Digit tmp;
+	tmp.digit = digit + x;
+	return tmp;
+}
+
+Digit Digit::operator-(const Digit x)
+{
+	Digit tmp;
+	tmp.digit = digit - x.digit;
+	return tmp;
+}
+
+Digit Digit::operator-(const int x)
+{
+	Digit tmp;
+	tmp.digit = digit - x;
+	return tmp;
+}
+
+Digit Digit::operator*(const Digit x)
+{
+	Digit tmp;
+	tmp.digit = digit * x.digit;
+	return tmp;
+	
+}
+
+Digit Digit::operator*(const int x)
+{
+	Digit tmp;
+	tmp.digit = digit * x;
+	return tmp;
+}
+
+Digit Digit::operator/(const Digit x)
+{
+	Digit tmp;
+	tmp.digit = digit / x.digit;
+	return tmp;
+
+}
+
+Digit Digit::operator/(const int x)
+{
+	Digit tmp;
+	tmp.digit = digit / x;
+	return tmp;
+}
+
+
+Digit Digit::operator%(const Digit x)
+{
+	Digit tmp;
+	tmp.digit = digit % x.digit;
+	return tmp;
+
+}
+
+Digit Digit::operator%(const int x)
+{
+	Digit tmp;
+	tmp.digit = digit % x;
+	return tmp;
+}
+
+bool Digit::operator>(const Digit &x)
+{
+	return digit > x.digit;
+}
+
+bool Digit::operator>( const int x)
+{
+	return digit > x;
+}
+
+bool Digit::operator<(const Digit &x)
+{
+	return digit < x.digit;
+}
+
+bool Digit::operator<(const int x)
+{
+	return digit < x;
+}
+
+bool Digit::operator==(const Digit &x)
+{
+	return digit == x.digit;
+}
+
+bool Digit::operator==(const int x)
+{
+	return digit == x;
+}
+
+bool Digit::operator!=(const Digit &x)
+{
+	return digit != x.digit;
+}
+
+bool Digit::operator!=(const int x)
+{
+	return digit != x;
+}
+
+bool Digit::operator>=(const Digit &x)
+{
+	return digit >= x.digit;
+}
+
+bool Digit::operator>=(const int x)
+{
+	return digit >= x;
+}
+
+bool Digit::operator<=(const Digit &x)
+{
+	return digit <= x.digit;
+}
+
+bool Digit::operator<=(const int x)
+{
+	return digit <= x;
+}
+
+Digit Digit::operator++(int)
+{
+	Digit tmp = *this;
+	tmp.digit++;
+	return tmp;
+
+}
+
+Digit Digit::operator++()
+{
+	digit = digit + 1;
+	return *this;
+}
+
+Digit Digit::operator--(int)
+{
+	Digit tmp = *this;
+	tmp.digit--;
+	return tmp;
+}
+
+Digit Digit::operator--( )
+{
+	digit = digit + 1;
+	return *this;
+}
+
+Digit Digit::operator=(const Digit &x)
+{
+	digit = x.digit;
+	return *this;
+}
+
+Digit Digit::operator=(Digit&&x)
+{
+	digit = x.digit;
+	x.digit = 0;
+	return *this;
+	
+}
+
