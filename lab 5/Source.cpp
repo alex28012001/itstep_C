6b7bd60fcc8f1219cad7f8c2829cdee53e2c030a
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-07 14:57:36 +0300

    Update Source.cpp

--- lab 5/Source.cpp
+++ lab 5/Source.cpp
@@ -4,29 +4,7 @@
 #include<iostream>
 int main()
 {
-	Digit a;
-	Digit b(4);
-	Digit c;
-	std::cout << (a + b).getDigit();
-	std::cout << "\n";
-	std::cout << (a + 5).getDigit();
-	std::cout << "\n";
-	std::cout << (a > b) << std::endl;
-	std::cout << Digit::getCounter() << std::endl;
-	a = b = c ;
-	
-	int x = a;
-	std::cout << x;
-	double y = c;
-	std::cout << y << std::endl;
-
-
-	c(1000);
-	std::cout << c.getDigit();
-
-	
 	Test::test();
-
 
 	return 0;
 }
