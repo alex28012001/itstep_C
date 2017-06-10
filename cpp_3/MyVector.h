e517080e81f92f60caa1bce9fb98b20e77a4e74d
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-10 15:22:21 +0300

    Create MyVector.h

--- cpp_3/MyVector.h
+++ cpp_3/MyVector.h
@@ -8,32 +8,7 @@ class MyVector
 
 public:
 	MyVector() : size(0), capacity(32) { arr = new double[capacity]; };
-	MyVector(double arr1[], int size1) :size(size1)
-	{
-		
-		int counter = size1 / 32;
-
-		arr = new double[(counter + 1) * 32];
-		capacity = 32 * (counter + 1);
-
-		for (unsigned int i = 0; i < size; ++i)
-			arr[i] = arr1[i];
-
-	}
-
-
-	MyVector(MyVector &a)
-	{
-		this->size = a.size;
-		this->capacity = a.capacity;
-		arr = new double[capacity];
-		for (int i = 0; i < size; i++)
-		{
-			arr[i] = a.arr[i];
-		}
-
-	};
-
+	MyVector(MyVector &a);
 	bool empty()const;
 	double get_size()const;
 	double get_capacity()const;
