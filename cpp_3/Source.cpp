2f5fac9eab1bd1dff58f668399612c7532ef4902
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-10 15:10:40 +0300

    Create Source.cpp

--- cpp_3/Source.cpp
+++ cpp_3/Source.cpp
@@ -1,32 +1,12 @@
 #include "MyVector.h"
+#include "Test.h"
 #include<iostream>
 
 int main()
 {
-	double arr1[11];
-	for (int i = 0; i < 11; ++i)
-	{
-		arr1[i] = rand() % 11;
-	}
-	MyVector a(arr1, 11);
-	std::cout << "Array: ";
-	a.print();
-	std::cout<<"Empty: "<<a.empty()<<"\n";
-	std::cout<<"Size: "<<a.get_size()<<"\n";
-	std::cout <<"Capacity: "<< a.get_capacity() << "\n";
-	std::cout << "Push back : ";
-	a.push_back(1);
-	a.print();
-	std::cout << "Pop back : ";
-	a.pop_back();
-	a.print();
-	std::cout << "Index(7): ";
-	std::cout << a.at(7) << "\n";
+	Test t;
+	t.test();
 	
-
-
-
-
 
 	return 0;
 }
