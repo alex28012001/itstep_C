2306d40e21aab2fb1ea4099d202c1c40f710e6d7
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-10 14:47:32 +0300

    Create Source.cpp

--- cpp_3/Source.cpp
+++ cpp_3/Source.cpp
@@ -3,15 +3,27 @@
 
 int main()
 {
-	MyVector abc;
-	MyVector abcd = abc;
-	std::cout << abc.empty() << std::endl;
-	std::cout << abc.get_size() << std::endl;
-	std::cout << abc.get_capacity() << std::endl;
-	abc.push_back(56);
-	std::cout << abc.pop_back() << std::endl;
-	abc.at(3);
-	abc.print();
+	double arr1[11];
+	for (int i = 0; i < 11; ++i)
+	{
+		arr1[i] = rand() % 11;
+	}
+	MyVector a(arr1, 11);
+	std::cout << "Array: ";
+	a.print();
+	std::cout<<"Empty: "<<a.empty()<<"\n";
+	std::cout<<"Size: "<<a.get_size()<<"\n";
+	std::cout <<"Capacity: "<< a.get_capacity() << "\n";
+	std::cout << "Push back : ";
+	a.push_back(1);
+	a.print();
+	std::cout << "Pop back : ";
+	a.pop_back();
+	a.print();
+	std::cout << "Index(7): ";
+	std::cout << a.at(7) << "\n";
+	
+
 
 
 
