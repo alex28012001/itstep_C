41fe7a71396b67075643044b5aa3166f6606cf9f
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-10 21:50:05 +0300

    Create MyVector.h

---
+++ lab 3/MyVector.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<iostream>
+class MyVector
+{
+	double * arr;
+	size_t size;
+	size_t capacity;
+
+public:
+	MyVector() : size(0), capacity(32) { arr = new double[capacity]; };
+	MyVector(const MyVector &a) : size(a.size), capacity(a.capacity)
+	{
+		arr = new double[capacity];
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = a.arr[i];
+		}
+	}
+	bool empty()const;
+	double get_size()const;
+	double get_capacity()const;
+	void push_back(double a);
+	double pop_back();
+	double& at(const int a);
+	void print()const;
+
+
+
+
+
+
+
+	~MyVector() { delete[]arr; };
+};
