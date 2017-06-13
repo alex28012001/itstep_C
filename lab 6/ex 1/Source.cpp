6c77d47a76e05e2ba0fd250d1ddedfeef701ad2c
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-13 16:06:02 +0300

    Create Source.cpp

--- lab 6/ex 1/Source.cpp
+++ lab 6/ex 1/Source.cpp
@@ -6,8 +6,15 @@ int main()
 	
 	Digit a(10);
 	Digit b(25);
-	std::cout<<(a += b).getDigit()<<"\n";
-	std::cout << (a += 100).getDigit()<<"\n";
+	std::cout << "Operator += : " << (a += b).getDigit() << "\n";
+	std::cout << "Operator(int) += : " << (a += 100).getDigit() << "\n";
+	std::cout << "Operator -= : " << (a -= b).getDigit() << "\n";
+	std::cout << "Operator(int) -= : " << (a -= 100).getDigit() << "\n";
+	std::cout << "Operator /= : " << (a /= b).getDigit() << "\n";
+	std::cout << "Operator(int) /= : " << (a /= 100).getDigit() << "\n";
+	std::cout << "Operator %= : " << (a %= b).getDigit() << "\n";
+	std::cout << "Operator(int) %= : " << (a %= 100).getDigit() << "\n";
+
 	int h = b;
 	std::cout << h << "\n";
 	double y = a;
