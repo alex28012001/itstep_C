db8dd05f26d6bc37045574d3b0743f4db7610c27
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-14 16:09:22 +0300

    Create Pair.h

--- lab 11/Pair.h
+++ lab 11/Pair.h
@@ -1,27 +1,28 @@
 #include<iostream>
+
 #pragma once
 template<typename T, typename U>
 class Pair
 {
 	T first;
 	U second;
-	
+
 
 public:
-	Pair():first(0), second(null) {};
+	Pair() :first(0), second(0) {};
 	Pair(const size_t x, const size_t y) : first(x), second(y) {};
 	T get_first()const;
 	U get_second()const;
 	void set_first(const size_t x);
 	void set_second(const size_t x);
-	
+
 
 
 	~Pair() {};
 };
 
 template<typename T, typename U>
-T Pair<T,U>::get_first()const
+T Pair<T, U>::get_first()const
 {
 	return first;
 }
@@ -46,3 +47,52 @@ void Pair<T, U>::set_second(const size_t x)
 
 
 
+
+
+
+
+
+
+template<>
+class Pair<bool, bool>
+{
+	bool first;
+	bool second;
+
+
+public:
+	Pair() :first(false), second(false) {};
+	Pair( const bool x,  const bool y) : first(x), second(y) {};
+	bool get_first()const;
+	bool get_second()const;
+	void set_first(const bool x);
+	void set_second(const bool x);
+
+
+
+	~Pair() {};
+};
+
+template<bool, bool>
+ bool Pair<bool, bool>::get_first()const
+{
+	return first;
+}
+
+ template<bool, bool>
+bool Pair<bool, bool>::get_second()const
+{
+	return second;
+}
+
+template<bool, bool>
+void Pair<bool, bool>::set_first(const bool x)
+{
+	first = x;
+}
+
+template<bool, bool>
+void Pair<bool, bool>::set_second(const bool x)
+{
+	second = x;
+}
