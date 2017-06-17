34f20c49d62eecaeef170e72c24e29ff3e10cb72
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-17 10:57:04 +0300

    Update Source.cpp

--- lab 8/Source.cpp
+++ lab 8/Source.cpp
@@ -4,8 +4,21 @@
 int main()
 {
 	Complex c1(5, 3);
-	Complex c2();
-
-
+	Complex c2(4,4);
+	std::cout <<" c1 :  "<< c1<<"\n";
+	std::cout <<" c2 :  "<< c2<<"\n\n";
+	std::cout << " +  :  " << (c1 + c2) << "\n";
+	std::cout << " -  :  " << (c1 - c2) << "\n";
+	std::cout << " *  :  " << (c1 * c2) << "\n";
+	std::cout << " /  :  " << (c1 / c2) << "\n";
+	std::cout << "+=  :  " << (c1 += c2) << "\n";
+	std::cout << "-=  :  " << (c1 -= c2) << "\n";
+	std::cout << "*=  :  " << (c1 *= c2) << "\n";
+	std::cout << "/=  :  " << (c1 /= c2) << "\n";
+	std::cout << "++  :  " << (++c1) << "\n";
+	std::cout << "--  :  " << (++c1) << "\n";
+	c1(100, 100);
+	std::cout << "()  :  " << c1<<"\n";
+;
 	return 0;
 }
