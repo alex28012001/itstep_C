8686db5070390ba75ee07b2fbb130eb6f8981250
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-17 10:56:50 +0300

    Update Complex.h

--- lab 8/Complex.h
+++ lab 8/Complex.h
@@ -1,12 +1,13 @@
 #pragma once
+#include<iostream>
 class Complex
 {
 	double real;
 	double img;
 
 public:
-	Complex(): real(0), img(0) {};
-	Complex(const double r,const double i) : real(r), img(i) {};
+	Complex() : real(0), img(0) {};
+	Complex(const double r, const double i) : real(r), img(i) {};
 	Complex(const Complex&a) : real(a.real), img(a.img) {};
 
 	~Complex() {};
@@ -16,6 +17,24 @@ public:
 	friend Complex operator+(const Complex&a, const Complex&b);
 	friend Complex operator-(const Complex&a, const Complex&b);
 	friend Complex operator*(const Complex&a, const Complex&b);
-
+	friend Complex operator/(const Complex& a, const Complex& b);
+	Complex& operator+=(const Complex a);
+	Complex& operator-=(const Complex a);
+	Complex& operator*=(const Complex a);
+	Complex& operator/=(const Complex a);
+	void operator()(const double r, const double i);
+	Complex& operator++();
+	Complex& operator++(int);
+	Complex& operator--();
+	Complex& operator--(int);
+	friend std::ostream& operator<<(std::ostream&, const Complex&);
+	friend std::istream& operator>>(std::istream&, Complex&);
+	friend Complex operator+(const Complex a, const double r);
+	friend Complex operator-(const Complex a, const double r);
+	friend Complex operator*(const Complex a, const double r);
+	friend Complex operator/(const Complex a, const double r);
+	friend Complex operator+(const double r, const Complex a);
+	friend Complex operator-(const double r, const Complex a);
+	friend Complex operator*(const double r, const Complex a);
+	friend Complex operator/(const double r, const Complex a);
 };
-
