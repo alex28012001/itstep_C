f9ad3da7c1592ab5c607ac26a86c4e638976e30a
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-06-29 21:16:16 +0300

    Update Pet.h

--- lab 18/ex 1/Pet.h
+++ lab 18/ex 1/Pet.h
@@ -1,9 +1,22 @@
 #pragma once
+#include<iostream>
+#include<string>
 class Pet
 {
 	std::string name;
+	int age;
+	std::string color;
+	std::string animal;
 public:
-	Pet() : name("Pet"){};
-	virtual ~Pet(){};
+	Pet(std::string x,int y, std::string c,std::string b) : name(x),age(y),color(c),animal(b){};
+	void show()
+	{
+		std::cout << name;
+	}
+	virtual void sound() = 0;
+	virtual void type()
+	{
+		std::cout << animal;
+	}
+	virtual ~Pet() {};
 };
-
