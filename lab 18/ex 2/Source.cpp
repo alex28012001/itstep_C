5b552f6debf89e07f2a62ea8893d41d31a785c95
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-03 13:25:48 +0300

    Update Source.cpp

--- lab 18/ex 2/Source.cpp
+++ lab 18/ex 2/Source.cpp
@@ -6,9 +6,9 @@ int main()
 	
 	Taxi obj;
 	Taxi obj2;
-	std::cout << obj.payment(120)<<"$\n";
-	std::cout << obj.payment(60) << "$\n";
-	std::cout << obj.get_counter() << "\n";
+	std::cout <<"Sum: "<< obj.payment(120)<<"$\n";
+	std::cout <<"Sum: "<< obj.payment(60) << "$\n";
+	std::cout <<"Ojects: "<< obj.get_counter() << "\n";
 	obj.getInfo();
 	std::cout<<"Amount_of_money: "<<obj.get_sumMoney()<<"$\n";
 	std::cout <<"Movement: "<< obj.move()<<"\n";
