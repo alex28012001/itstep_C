d3b26522fa6cbab93e337983737a49426ea30ecf
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-12 16:38:56 +0300

    Create Rectangle.h

---
+++ lab 21/ex A/Rectangle.h
@@ -0,0 +1,12 @@
+#include "Parallelogram.h"
+class Rectangle : public Parallelogram
+{
+	double d1;
+	double d2;
+	double angle;
+public:
+	Rectangle(const double d1, const double d2, const double angle) : Parallelogram(10,5,30), d1(d1), d2(d2), angle(angle) {};
+	double getArea();
+	void info();
+	~Rectangle(){};
+};
