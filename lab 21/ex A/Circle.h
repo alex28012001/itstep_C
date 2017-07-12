6efe954bc7fd57eeb6b54fab705c7b2473110e99
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-12 22:16:29 +0300

    Update Circle.h

--- lab 21/ex A/Circle.h
+++ lab 21/ex A/Circle.h
@@ -1,15 +1,15 @@
 #pragma once
 #include"ShapeA.h"
 #include<iostream>
-class Square : public Shape
+class Circle: public Shape
 {
 	double radius;
 	double x;
 	double y;
 
 public:
-	Square(const double r,const double x,const double y) : radius(r),x(x),y(y) {};
+	Circle(const double r,const double x,const double y) : radius(r),x(x),y(y) {};
 	double getArea();
 	void info();
-	~Square() {};
+	~Circle() {};
 };
