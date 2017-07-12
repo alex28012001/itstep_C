0627aad400c26351ce96561cd0cde4e108612514
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-12 16:38:26 +0300

    Create Parallelogram.h

---
+++ lab 21/ex A/Parallelogram.h
@@ -0,0 +1,12 @@
+#include"ShapeA.h"
+class Parallelogram : virtual public Shape
+{
+	double a;
+	double b;
+	double angle;
+public:
+	Parallelogram(const double a, const double b, const double angle) : a(a), b(b), angle(angle){};
+	double getArea() override;
+	void info() override;
+	~Parallelogram(){};
+};
