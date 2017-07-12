d1c7da09fa15f2c61d38547dcec33d94f044bed2
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-12 21:17:14 +0300

    Update Rectangle.h

--- lab 21/ex A/Rectangle.h
+++ lab 21/ex A/Rectangle.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "Parallelogram.h"
 class Rectangle : public Parallelogram
 {
@@ -5,8 +6,8 @@ class Rectangle : public Parallelogram
 	double d2;
 	double angle;
 public:
-	Rectangle(const double d1, const double d2, const double angle) : Parallelogram(10,5,30), d1(d1), d2(d2), angle(angle) {};
+	Rectangle(const double d1, const double d2, const double angle) : Parallelogram(10, 5, 30), d1(d1), d2(d2), angle(angle) {};
 	double getArea();
 	void info();
-	~Rectangle(){};
+	~Rectangle() {};
 };
