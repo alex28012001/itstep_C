d321a0242a675e8afa611ae3e7f00e07852eb20e
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-13 19:54:25 +0300

    Update Triangle.h

--- lab 21/ex A/Triangle.h
+++ lab 21/ex A/Triangle.h
@@ -15,9 +15,15 @@ public:
 	Triangle(const double a, const double b, const double c);
 	Triangle(std::pair<double, double> x, const double angle);
 	Triangle(const double angle, std::pair<double, double> x);
+	void set_a(const double x);
+	void set_b(const double x);
+	void set_c(const double x);
+	void set_angle_a(const double x);
+	void set_angle_b(const double x);
+	void set_angle_c(const double x);
 	double getArea() override;
 	void info() override;
-		
+
 
 	~Triangle() {};
 };
