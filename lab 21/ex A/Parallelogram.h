4501e01ddabdb2c0bce15faf774906ed81c1618c
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-13 19:54:46 +0300

    Update Parallelogram.h

--- lab 21/ex A/Parallelogram.h
+++ lab 21/ex A/Parallelogram.h
@@ -7,6 +7,9 @@ class Parallelogram : virtual public Shape
 	double angle;
 public:
 	Parallelogram(const double a, const double b, const double angle) : a(a), b(b), angle(angle) {};
+	void set_a(const double x);
+	void set_b(const double x);
+	void set_angle(const double x);
 	double getArea() override;
 	void info() override;
 	virtual ~Parallelogram() {};
