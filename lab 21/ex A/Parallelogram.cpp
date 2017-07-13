786c2c80c9209621e5d87cfc22c34bee000f441f
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-13 19:54:59 +0300

    Update Parallelogram.cpp

--- lab 21/ex A/Parallelogram.cpp
+++ lab 21/ex A/Parallelogram.cpp
@@ -3,6 +3,21 @@
 #include<cmath>
 #define PI 3.14159265  
 
+void Parallelogram::set_a(const double x)
+{
+	a = x;
+
+}
+void Parallelogram::set_b(const double x)
+{
+	b = x;
+}
+
+void Parallelogram::set_angle(const double x)
+{
+	angle = x;
+}
+
 double Parallelogram::getArea()
 {
 	std::cout << "Area parallelogramm : ";
