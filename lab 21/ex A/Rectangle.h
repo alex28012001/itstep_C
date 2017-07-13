815c419fc9fc4ce929c116a644167c1baa7db3c8
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-13 19:55:31 +0300

    Update Rectangle.h

--- lab 21/ex A/Rectangle.h
+++ lab 21/ex A/Rectangle.h
@@ -7,6 +7,9 @@ class Rectangle : public Parallelogram
 	double angle;
 public:
 	Rectangle(const double d1, const double d2, const double angle) : Parallelogram(10, 5, 30), d1(d1), d2(d2), angle(angle) {};
+	void set_d1(const double x);
+	void set_d2(const double x);
+	void set_angle(const double x);
 	double getArea();
 	void info();
 	~Rectangle() {};
